157284919194e87c220c9ee8430eabf836215fd0
Chantel Wetzel - NOAA Federal <chantel.wetzel@noaa.gov>
2024-12-18 13:57:23 -0800

    update the survey biological data object

--- data-raw/data_survey.R
+++ data-raw/data_survey.R
@@ -1,21 +1,41 @@
-get_data_survey()
+#get_data_survey()
 
 data_survey_catch <-
   fs::dir_ls(here::here("data-raw", "survey", "trawl"), regex = "catch_") |>
   purrr::map(
     .f = function(x) {load(x); return(x)}
   )
-
-data_survey_bio <- fs::dir_ls(here::here("data-raw", "survey", "trawl"), regex = "bio_") |>
+#data_survey_bio <- fs::dir_ls(here::here("data-raw", "survey", "trawl"), regex = "bio_")[2:3] |>
+#  purrr::map(
+#    .f = function(x) {
+#      load(x)
+#      if (inherits(x, "list")) {
+#        data <- dplyr::bind_rows(x)
+#      }
+#      return(data)
+#    }
+#  )
+nwfsc_combo_bio <- fs::dir_ls(here::here("data-raw", "survey", "trawl"), regex = "bio_")[2] |>
+  purrr::map(
+    .f = function(x) {
+    load(x)
+    return(x)
+  })
+nwfsc_slope_bio <- fs::dir_ls(here::here("data-raw", "survey", "trawl"), regex = "bio_")[3] |>
   purrr::map(
     .f = function(x) {
       load(x)
-      if (inherits(x, "list")) {
-        data <- dplyr::bind_rows(x)
-      }
-      return(data)
-    }
-  )
+      return(x)
+    })
+load(fs::dir_ls(here::here("data-raw", "survey", "trawl"), regex = "bio_")[4]) 
+tri_bio <- x
+load(fs::dir_ls(here::here("data-raw", "survey", "trawl"), regex = "bio_")[1]) 
+afsc_bio <- x
+data_survey_bio <- list()
+data_survey_bio$nwfsc_combo <- nwfsc_combo_bio 
+data_survey_bio$nwfsc_slope <- nwfsc_slope_bio 
+data_survey_bio$afsc_slope <- afsc_bio 
+data_survey_bio$triennial <- tri_bio
 
 usethis::use_data(
   data_survey_catch,
