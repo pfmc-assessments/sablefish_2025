48fe4214ded5cca258432f6668fe3d38b43e9255
Chantel Wetzel - NOAA Federal <chantel.wetzel@noaa.gov>
2025-01-07 06:34:22 -0800

    add initial bds processing script

---
+++ R/process_bds.R
@@ -0,0 +1,145 @@
+#' Process PacFIN bds data
+#'
+#' @param save_dir The path to the assessment package of interest. The default
+#'   uses [here::here()] to locate the appropriate directory but you can set it
+#'   to whatever directory you want as long as that directory contains
+#'   `data-raw`.
+#' @param bds_file File name of the PacFIN bds file to process. 
+#' @param catch_file Name of the catch file to be used to expand biological data.
+#'   This file should be located within `data-processed` with the `here` package
+#'   used to find the file in the directory.
+#' @param age_bins Numerical sequence of age bins to process age data.
+#' @param length_bins  Numerical sequence of length bins to process length data.
+#' @param gears Gear types to group the data within.  This should align with 
+#'   PacFIN gear names.
+#' @param species_code String that is used to name the saved length and age 
+#'   composition files.
+#' @param common_name
+#' 
+#' @export
+process_bds_data <- function(
+  save_dir,
+  bds_file,
+  catch_file = "data_commercial_catch_expansions.csv",
+  age_bins = age_bins,
+  length_bins = len_bins,
+  gears = gears,
+  species_code = "SABL",
+  common_name = common_name) {
+  # bds.pacfin
+  file_bds <- fs::dir_ls(here::here("data-raw", "bds"), regex = bds_file)
+  load(file_bds)
+  # pre-processed catch data by year, state, and geargroup
+  file_catch <- here::here("data-processed",  catch_file)
+  
+  # The maximum quantile at which to cap all data expansions within getExpansion_1().
+  expansion <- 0.95
+  
+  # Determine what samples to retain or filter out when using cleanPacFIN()
+  # Keep all alternate (A), fork (F), and unknown (U) length types based on 
+  # FISH_LENGTH_TYPE_CODE. This decision should be species-specific
+  good_lengths <- c("U", "A", "F")
+  # Keep only random (R) samples based on SAMPLE_METHOD_CODE. Only random samples
+  # should be retained unless specified by a state agency.
+  good_methods <- "R"
+  # Keep commercial on-board (C), market (M), and blank samples based on SAMPLE_TYPE
+  good_samples <- c("", "M", "C")
+  # Keep data from all 3 states. This is the default.
+  good_states <- c("WA", "OR", "CA")
+  # Keep only break and burn (B, BB), unknown (U), and blank (") age reads based 
+  # on AGE_METHOD. This decision should be species-specific.
+  good_age_method <- c("B", "BB", "U", "")
+  # TODO: evaluate all available age methods types. 
+  # 1. What is the prevalence of each? state? years?
+  # 2. There are a large number ages with blank (or NA) age method primarily in 
+  # Oregon. Make sure these ages look similar to those that are marked BB.  These
+  # ages should ideally be kept due to the number of samples.
+  # 3. Determine if any age method types should be kept beyond the default of BB. 
+  # Often S age methods are tossed, should we do that here?
+  
+  # Pull survey data 
+  bds_survey <- nwfscSurvey::pull_bio(
+    common_name = common_name,
+    survey = "NWFSC.Combo"
+  )
+  # Estimate weight-length relationship by sex
+  weight_length_estimates <- nwfscSurvey::estimate_weight_length(
+    bds_survey,
+    verbose = FALSE
+  )
+  
+  catch_formatted <- utils::read.csv(file_catch) |>
+    formatCatch(
+      strat = c("state", "geargroup"),
+      valuename = "catch_mt"
+    )
+  
+  bds_cleaned <- cleanPacFIN(
+    Pdata = bds.pacfin,
+    keep_gears = used_gears,
+    CLEAN = TRUE,
+    keep_age_method = good_age_method,
+    keep_sample_type = good_samples,
+    keep_sample_method = good_methods,
+    keep_length_type = good_lengths,
+    keep_states = good_states,
+    spp = common_name,
+    savedir = save_dir
+  ) |>
+    dplyr::mutate(
+      stratification = paste(state, geargroup, sep = ".")
+    )
+  save(bds_cleaned, here::here("data-raw", "bds", 
+                               paste0("cleaned_", bds_file)))
+  # TODO: 
+  # 1. determine if any data filtering should be done to remove outliers by plotting
+  # age and length comparisons.  The nwfscSurvey::est_growth function can help with this.
+  # 2. evaluate whether the correct age methods are returned and whether they 
+  # are appropriate.
+  
+  
+  expanded_comps <- get_pacfin_expansions(
+    Pdata = bds_cleaned,
+    Catch = catch_formatted,
+    weight_length_estimates = weight_length_estimates,
+    Units = "MT",
+    maxExp = expansion,
+    Exp_WA = TRUE,
+    verbose = TRUE,
+    savedir = here::here("data-raw", "bds"))
+  
+  length_comps_long <- getComps(
+    Pdata = dplyr::filter(expanded_comps, !is.na(lengthcm)),
+    Comps = "LEN",
+    weightid = "Final_Sample_Size_L"
+  )
+  
+  length_composition_data <- writeComps(
+    inComps = length_comps_long,
+    fname = fs::path(
+      here::here("data-raw", "bds"),
+      glue::glue("{species_code}_lcomps_{min(length_bins)}-{max(length_bins)}.csv")
+    ),
+    comp_bins = length_bins,
+    verbose = TRUE
+  ) 
+  
+  age_comps_long <- getComps(
+    Pdata =  dplyr::filter(expanded_comps, Age != -1),
+    Comps = "AGE",
+    weightid = "Final_Sample_Size_A"
+  )
+  
+  age_composition_data <- writeComps(
+    inComps = age_comps_long,
+    fname = fs::path(
+      here::here("data-raw", "bds"),
+      glue::glue("{species_code}_acomps_{min(age_bins)}-{max(age_bins)}.csv")
+    ),
+    comp_bins = age_bins,
+    verbose = TRUE
+  )
+
+}
+
+
