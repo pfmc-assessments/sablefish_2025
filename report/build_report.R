0f10af50d73e1e72acbd4f6dbbbbc8a81bd5084c
Chantel Wetzel - NOAA Federal <chantel.wetzel@noaa.gov>
2025-01-07 06:28:14 -0800

    initial document creation script

---
+++ report/build_report.R
@@ -0,0 +1,30 @@
+pak::pkg_install("nmfs-ost/asar")
+pak::pkg_install("nmfs-ost/satf")
+
+# Read in the output from an SS3 and create a csv file with estimates:
+# I have created the relative directory of the project where there is
+# folder called "model" where the SS3 model files are.  This directory
+# is passed to the function in the `outdir` and `savedir` arguements below:
+asar::convert_output(
+  output_file = "Report.sso", 
+  outdir = here::here("model", "_bridging", "0_2023_model"),
+  model = "ss3", 
+  file_save = TRUE,
+  savedir = here::here("report"),
+  save_name = "sablefish_convert_output"
+)
+
+asar::create_template(
+  format = "pdf",
+  office = "NWFSC",
+  region = "U.S. West Coast",
+  species = "sablfefish",
+  spp_latin = "Anoplopoma fimbria",
+  year = 2025,
+  author = c("Chantel R. Wetzel", "Aaron M. Berger", "Cheryl Barnes", "Joshua A. Zahner", "Nick Tolimieri"),
+  include_affiliation = TRUE,
+  simple_affiliation = FALSE,
+  resdir = here::here("report"),
+  model_results = "sablefish_convert_output.csv",
+  model = "SS3"
+)
