9b19e1c9881dca2cda3c790b042c6fb7136d1765
Chantel Wetzel - NOAA Federal <chantel.wetzel@noaa.gov>
2025-01-17 10:19:52 -0800

    add util fxn for binding lists

--- R/utils.R
+++ R/utils.R
@@ -85,3 +85,14 @@ read_all_models_fast <- function(dir, ...) {
   )
   invisible(return(output))
 }
+
+bind_compositions <- function(x) {
+  stopifnot(inherits(x, "list"))
+  good_names <- colnames(x[[1]])
+  one_data_frame <- purrr::map_dfr(x, \(y) setNames(y, good_names))
+  old_names <- colnames(one_data_frame)
+  new_names <- gsub("^[UMF]([0-9]+)\\.*[0-9]*$", "U\\1", old_names)
+  no_dots <- gsub("\\.{3}[0-9]+$", "", new_names)
+  colnames(one_data_frame) <- no_dots
+  return(one_data_frame)
+}
