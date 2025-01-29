1bc3b0203f3aa4074c72cbbf80b179bb1b70a4d0
Chantel Wetzel - NOAA Federal <chantel.wetzel@noaa.gov>
2025-01-29 07:46:55 -0800

    fix: survey comps

--- data-raw/data_survey.R
+++ data-raw/data_survey.R
@@ -24,10 +24,10 @@ nwfsc_slope_bio <- x
 load(fs::dir_ls(here::here("data-raw", "survey", "trawl"), regex = "bio_")[4]) 
 tri_bio_early <- x
 tri_bio_early$length_data <- tri_bio$length_data |> dplyr::filter(Year >= 1980, Year <= 1992)
-tri_bio_early$age_data <- tri_bio$length_data |> dplyr::filter(Year >= 1980, Year <= 1992)
+tri_bio_early$age_data <- tri_bio$age_data |> dplyr::filter(Year >= 1980, Year <= 1992)
 tri_bio_late <- x
 tri_bio_late$length_data <- tri_bio$length_data |> dplyr::filter(Year > 1992)
-tri_bio_late$age_data <- tri_bio$length_data |> dplyr::filter(Year > 1992)
+tri_bio_late$age_data <- tri_bio$age_data |> dplyr::filter(Year > 1992)
 load(fs::dir_ls(here::here("data-raw", "survey", "trawl"), regex = "bio_")[1]) 
 afsc_bio <- x
 afsc_bio$length_data <- afsc_bio$length_data |> dplyr::filter(Year >= 1997)
