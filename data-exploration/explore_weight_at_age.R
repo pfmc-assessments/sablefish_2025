54ff371c568ea76c90dd48efee0cd1e95c89bb47
Chantel Wetzel - NOAA Federal <chantel.wetzel@noaa.gov>
2025-04-02 05:48:12 -0700

    add weight-at-age comparison figures

--- data-exploration/explore_weight_at_age.R
+++ data-exploration/explore_weight_at_age.R
@@ -66,6 +66,124 @@ ggplot() +
   ) +
   facet_grid(sex~.)
 
+#===============================================================================
+# Compare estimates when using survey or survey ang fishery data
+#===============================================================================
+
+library(r4ss)
+library(ggplot2)
+
+wtatage_model_survey <- r4ss::SS_readwtatage(
+  file = here::here("data-processed", "wtatage_model_biomass_weighted.ss")) |>
+  dplyr::filter(fleet == 1) |>
+  dplyr::select(-seas, -bio_pattern, -birthseas) |>
+  tidyr::pivot_longer(
+    cols = 4:34,
+    names_to = "age",
+    values_to = "weight" 
+  ) |>
+  dplyr::filter(year < 2025, year > 2002) |>
+  dplyr::mutate(
+    sex = dplyr::case_when(sex == 1 ~ "Female", .default = "Male"),
+    age = as.numeric(age),
+    Year = as.factor(year),
+    Source = "Survey"
+  )
+
+wtatage_model_fishery_survey <- r4ss::SS_readwtatage(
+  file = here::here("data-processed", "wtatage_model_biomass_weighted_fishery_survey.ss")) |>
+  dplyr::filter(fleet == 1) |>
+  dplyr::select(-seas, -bio_pattern, -birthseas) |>
+  tidyr::pivot_longer(
+    cols = 4:34,
+    names_to = "age",
+    values_to = "weight" 
+  ) |>
+  dplyr::filter(year < 2025, year > 2002) |>
+  dplyr::mutate(
+    sex = dplyr::case_when(sex == 1 ~ "Female", .default = "Male"),
+    age = as.numeric(age),
+    Year = as.factor(year),
+    Source = "Fishery & Survey"
+  )
+
+data <- dplyr::bind_rows(
+  wtatage_model_survey, wtatage_model_fishery_survey
+)
+
+ggplot() +
+  geom_line(data = data, aes(x = age, y = weight, color = Year, linetype = Source), linewidth = 1.0) +
+  scale_color_viridis_d() +
+  xlab("Age (years)") + ylab("Weight (kg)") +
+  theme_bw() +
+  theme(
+    text = element_text(size = 15),
+    axis.text = element_text(size = 15)
+  ) +
+  facet_grid(sex~.)
+
+
+ggplot() +
+  geom_line(data = data |> dplyr::filter(Year %in% c(2003:2008)), 
+            aes(x = age, y = weight, color = sex, linetype = Source), linewidth = 1.0) +
+  scale_color_viridis_d() +
+  xlab("Age (years)") + ylab("Weight (kg)") +
+  theme_bw() +
+  theme(
+    text = element_text(size = 15),
+    axis.text = element_text(size = 15)
+  ) +
+  facet_wrap("Year")
+
+ggplot() +
+  geom_line(data = data |> dplyr::filter(Year %in% c(2009:2014)), 
+            aes(x = age, y = weight, color = sex, linetype = Source), linewidth = 1.0) +
+  scale_color_viridis_d() +
+  xlab("Age (years)") + ylab("Weight (kg)") +
+  theme_bw() +
+  theme(
+    text = element_text(size = 15),
+    axis.text = element_text(size = 15)
+  ) +
+  facet_wrap("Year")
+
+ggplot() +
+  geom_line(data = data |> dplyr::filter(Year %in% c(2015:2020)), 
+            aes(x = age, y = weight, color = sex, linetype = Source), linewidth = 1.0) +
+  scale_color_viridis_d() +
+  xlab("Age (years)") + ylab("Weight (kg)") +
+  theme_bw() +
+  theme(
+    text = element_text(size = 15),
+    axis.text = element_text(size = 15)
+  ) +
+  facet_wrap("Year")
+
+ggplot() +
+  geom_line(data = data |> dplyr::filter(Year %in% c(2021:2024)), 
+            aes(x = age, y = weight, color = sex, linetype = Source), linewidth = 1.0) +
+  scale_color_viridis_d() +
+  xlab("Age (years)") + ylab("Weight (kg)") +
+  theme_bw() +
+  theme(
+    text = element_text(size = 15),
+    axis.text = element_text(size = 15)
+  ) +
+  facet_wrap("Year")
+
+ggplot() +
+  geom_line(data = data |> dplyr::filter(Year %in% c(2018)), 
+            aes(x = age, y = weight, color = sex, linetype = Source), linewidth = 1.0) +
+  scale_color_viridis_d() +
+  xlab("Age (years)") + ylab("Weight (kg)") +
+  theme_bw() +
+  theme(
+    text = element_text(size = 15),
+    axis.text = element_text(size = 15)
+  ) +
+  facet_grid(Year~.)
+
+
 
 
 
