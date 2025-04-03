22a37bfb530bdbcde2c4ccabf2f9b9b1b6b8efbe
Chantel Wetzel - NOAA Federal <chantel.wetzel@noaa.gov>
2025-04-03 07:58:56 -0700

    add call to create figures

--- data-raw/model_output_objects.R
+++ data-raw/model_output_objects.R
@@ -2,6 +2,7 @@ model_name <- "13_m_prior"
 model_dir <- here::here("model", "_bridging", model_name)
 model_output <- r4ss::SS_output(model_dir)
 save(model_output, file = file.path(model_dir, "model_output.rda"))
+r4ss::SS_plots(model_output)
 
 write_r4ss_tables(
   replist = model_output,
