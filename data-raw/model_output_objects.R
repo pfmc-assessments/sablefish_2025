74bc15a2f2150c104529459a51394a074e05c907
Chantel Wetzel - NOAA Federal <chantel.wetzel@noaa.gov>
2025-04-07 14:36:11 -0700

    update table script

--- data-raw/model_output_objects.R
+++ data-raw/model_output_objects.R
@@ -6,20 +6,11 @@ save(model_output, file = file.path(model_dir, "model_output.rda"))
 r4ss::SS_plots(model_output)
 
 devtools::load_all("C:/Users/chantel.wetzel/Documents/github/r4ss")
-table_exec_summary(
+table_all(
   replist = model_output,
   fleetnames = c(
     "Trawl", "Hook-and-Line", "Pot",
     "Triennial", "AKFSC Slope", "NWFSC Slope", "WCGBT"
   )
 )
-pars <- table_pars(
-  output = model_output
-)
-rownames(pars) <- NULL
-save(pars, file = file.path(model_output$inputs$dir, "tables", "parameters.rda"))
-
-n_pars <- table_parcounts(
-  output = model_output
-)
-save(n_pars, file = file.path(model_output$inputs$dir, "tables", "n_parameters.rda"))
+table_config(replist = model_output)
