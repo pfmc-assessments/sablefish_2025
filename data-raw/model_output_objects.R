40b3ebe1e30b755242e14fe3586ad38136cf307b
Chantel Wetzel - NOAA Federal <chantel.wetzel@noaa.gov>
2025-04-17 07:01:32 -0700

    add info about the branch being used

--- data-raw/model_output_objects.R
+++ data-raw/model_output_objects.R
@@ -5,6 +5,9 @@ model_output <- r4ss::SS_output(model_dir)
 save(model_output, file = file.path(model_dir, "model_output.rda"))
 r4ss::SS_plots(model_output)
 
+# Alternative install the table branch for r4ss, should be merged soon into
+# the main branch and at which point everyone will need to update the 
+# package version.
 devtools::load_all("C:/Users/chantel.wetzel/Documents/github/r4ss")
 table_all(
   replist = model_output,
