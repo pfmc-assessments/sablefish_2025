bba76b48292af624ce9ea1fc56941807f71d9227
Chantel Wetzel - NOAA Federal <chantel.wetzel@noaa.gov>
2025-04-17 06:05:41 -0700

    check discard rates against the GEMM

--- data-exploration/explore_wcgop_discard.R
+++ data-exploration/explore_wcgop_discard.R
@@ -123,6 +123,146 @@ nwfscSurvey::plot_comps(
   add_save_name = "Pot_coastwide"
 )
 
+#===============================================================================
+# Compare model and GEMM rates
+#===============================================================================
+discard_rates_2025 <- read.csv(here::here(
+  file.path("data-processed", "data_commercial_discard_rates.csv"))) |>
+  dplyr::mutate(
+    fleet = dplyr::case_match(fleet, 2 ~ "HKL",
+                              3 ~ "Pot",
+                              1 ~ "Trawl"),
+    lwr = qnorm(0.025, discard_rate, sd),
+    upr = qnorm(0.975, discard_rate, sd),
+    assessment = as.factor(2025)
+  ) |>
+  dplyr::mutate(
+    lwr = dplyr::case_when(lwr < 0 ~ 0, .default = lwr),
+    year_offset = year
+  )
+
+data <- r4ss::SS_readdat(
+  "C:/Assessments/2025/sablefish_2025/model/_bridging/0_2023_model/data.ss"
+)
+discard_rates_2023 <- data$discard_data |>
+  dplyr::mutate(
+    fleet = dplyr::case_match(fleet, 2 ~ "Trawl", 1 ~ "HKL"),
+    lwr = qnorm(0.025, obs, stderr),
+    upr = qnorm(0.975, obs, stderr),
+    assessment = as.factor(2023)
+  ) |>
+  dplyr::mutate(
+    lwr = dplyr::case_when(lwr < 0 ~ 0, .default = lwr),
+    year_offset = year + 0.5
+  ) |>
+  dplyr::rename(
+    discard_rate = obs,
+    sd = stderr
+  )
+
+discard_rates_2023 <- dplyr::bind_rows(
+  discard_rates_2023, 
+  discard_rates_2023 |>
+    dplyr::filter(fleet == "HKL") |>
+    dplyr::mutate(
+      fleet = "Pot"
+    )
+)
+
+discard_rates <- dplyr::bind_rows(
+  discard_rates_2023,
+  discard_rates_2025
+)
+
+ggplot(discard_rates |> dplyr::filter(year > 2000), aes(x = year_offset, y = discard_rate, color = assessment)) +
+  geom_point() +
+  ggplot2::geom_errorbar(
+    ggplot2::aes(ymin = lwr, ymax = upr)) +
+  theme_bw() +
+  scale_color_viridis_d() +
+  ylab("Discard Rates") + xlab("Year") +
+  facet_grid("fleet")
+
+gemm_data <- nwfscSurvey::pull_gemm(common_name = "sablefish") |>
+  dplyr::filter(!sector %in%  c("Research", 
+                                "Washington Recreational",
+                                "Oregon Recreational",
+                                "California Recreational")) |>
+  dplyr::mutate(
+    catch_shares = "non-catch-shares",
+    fleet = NA
+  )
+catch_shares <- c(
+  "At-Sea Hake CP",
+  "At-Sea Hake MSCV",
+  "CS - Bottom and Midwater Trawl",
+  "CS - Bottom Trawl",
+  "CS - Hook & Line",
+  "CS - Pot",
+  "CS EM - Bottom Trawl",
+  "CS EM - Pot",
+  "Midwater Hake",
+  "Midwater Hake EM",
+  "Midwater Rockfish",
+  "Midwater Rockfish EM"
+)
+gemm_data[which(gemm_data[, "sector"] %in% catch_shares & gemm_data[, "year"] >= 2011), "catch_shares"] <- "catch-shares"
+pot <- c(
+  "CS - Pot",
+  "CS EM - Pot",
+  "LE Sablefish - Pot",
+  "LE Fixed Gear DTL - Pot",
+  "OA Fixed Gear - Pot"
+)
+hkl <- c(
+  "Combined LE & OA CA Halibut",
+  "CS - Hook & Line",
+  "Directed P Halibut",
+  "Incidental",
+  "LE CA Halibut",
+  "LE Fixed Gear DTL - Hook & Line",
+  "LE Sablefish - Hook & Line",
+  "Nearshore",
+  "OA CA Halibut",
+  "OA Fixed Gear - Hook & Line"
+)
+trawl <- c(
+  "At-Sea Hake CP",
+  "At-Sea Hake MSCV",
+  "Midwater Hake",
+  "Midwater Hake EM",
+  "Shoreside Hake",
+  "Tribal At-Sea Hake",
+  "CS - Bottom and Midwater Trawl",
+  "CS - Bottom Trawl",
+  "CS EM - Bottom Trawl",
+  "Limited Entry Trawl",
+  "Midwater Rockfish",
+  "Midwater Rockfish EM",
+  "Pink Shrimp",
+  "Research",
+  "Tribal Shoreside"
+)
+gemm_data[which(gemm_data[, "sector"] %in% hkl), "fleet"] <- "HKL"
+gemm_data[which(gemm_data[, "sector"] %in% trawl), "fleet"] <- "Trawl"
+gemm_data[which(gemm_data[, "sector"] %in% pot), "fleet"] <- "Pot"
+gemm_totals <- gemm_data |>
+  dplyr::group_by(year, fleet) |>
+  dplyr::summarize(
+    discard_mt = round(sum(total_discard_mt),1),
+    landed_mt = round(sum(total_landings_mt), 1),
+    catch = round(sum(total_discard_with_mort_rates_applied_and_landings_mt), 1),
+    gemm_discard_rate = discard_mt / (landed_mt + discard_mt)
+  ) 
+ggplot() +
+  geom_point(data = gemm_totals, aes(x = year, y = gemm_discard_rate, color = "GEMM", shape = "GEMM"), size = 3) +
+  geom_point(data = discard_rates_2025 |> dplyr::filter(year > 2000), aes(x = year, y = discard_rate, color = "Assessment", shape = "Assessment"), size = 3) +
+  theme_bw() +
+  scale_color_viridis_d(begin = 0, end = 0.5) +
+  ylab("Discard Rates") + xlab("Year") +
+  facet_grid("fleet")
+
+
 
 #===============================================================================
 # Look at the data by area north and south
