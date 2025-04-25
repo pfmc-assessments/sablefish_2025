ff9d7d733453d11b40685ef874fb615b8f7ef9be
Chantel Wetzel - NOAA Federal <chantel.wetzel@noaa.gov>
2025-04-25 13:51:11 -0700

    add years to plot and document

--- R/plot_comp_fits.R
+++ R/plot_comp_fits.R
@@ -79,21 +79,31 @@ plot_ghostfleets <- function(
   )
 }
 
-#' Create sexed aggregated fits to length and age data (e.g., do not include unsexed data)
+#' Create sexed aggregated fits to marginal age data (e.g., do not include unsexed data)
 #'
 #' @param  replist List of model objects created by `r4ss::SS_output()`.
-#' @param  common_name Common name of the species in the GEMM database.
 #' @param  dir Directory to save figures. The full directory is required and if 
 #'   provided a folder will be created in this location where figures will be 
 #'   saved.  The default is NULL which will create a folder to save special 
 #'   figures inside the replist directory.
+#' @param  years A vector of continous years to plot the aggregated fits for. Providing
+#'   a vector of years to plot can be useful to investigate fits for a subset of years
+#'   (e.g., block periods). The default is NULL which will plot the aggregated fleets for all years.
 #'
 #' @author Chantel Wetzel
 #' @export
 #'
-plot_fits_sexed_only <- function(
+plot_age_fits_sexed_only <- function(
   replist,
-  dir = NULL) {
+  dir = NULL,
+  years = NULL) {
+  
+  if (is.null(years)) {
+    years <- replist[["startyr"]]:replist[["endyr"]]
+    year_name <- "_all_years"
+  } else {
+    year_name <- paste0("_", min(years), "-", max(years))
+  }
   
   plot_dir <- file.path(
     ifelse(
@@ -101,13 +111,13 @@ plot_fits_sexed_only <- function(
       yes = replist[["inputs"]][["dir"]],
       no = dir
     ),
-    "sexed_aggregated_fits"
+    paste0("sexed_age_aggregated_fits", year_name)
   )
   dir.create(plot_dir)
   
   model_mod <- replist
   model_mod$agedbase <- model_mod$agedbase |> 
-    dplyr::filter(Sexes == 3)
+    dplyr::filter(Sexes == 3, Yr %in% years)
   SSplotComps(
     replist = model_mod,
     subplots = 21,
@@ -115,6 +125,43 @@ plot_fits_sexed_only <- function(
     print = TRUE,
     plotdir = plot_dir
   )
+}
+
+#' Create sexed aggregated fits to length data (e.g., do not include unsexed data)
+#'
+#' @param  replist List of model objects created by `r4ss::SS_output()`.
+#' @param  dir Directory to save figures. The full directory is required and if 
+#'   provided a folder will be created in this location where figures will be 
+#'   saved.  The default is NULL which will create a folder to save special 
+#'   figures inside the replist directory.
+#' @param  years A vector of continuous years to plot the aggregated fits for. Providing
+#'   a vector of years to plot can be useful to investigate fits for a subset of years
+#'   (e.g., block periods). The default is NULL which will plot the aggregated fleets for all years.
+#'
+#' @author Chantel Wetzel
+#' @export
+#'
+plot_len_fits_sexed_only <- function(
+    replist,
+    dir = NULL,
+    years = NULL) {
+  
+  if (is.null(years)) {
+    years <- replist[["startyr"]]:replist[["endyr"]]
+    year_name <- "_all_years"
+  } else {
+    year_name <- paste0("_", min(years), "-", max(years))
+  }
+  
+  plot_dir <- file.path(
+    ifelse(
+      is.null(dir),
+      yes = replist[["inputs"]][["dir"]],
+      no = dir
+    ),
+    paste0("sexed_len_aggregated_fits", year_name)
+  )
+  dir.create(plot_dir)
   
   model_mod$lendbase <- model_mod$lendbase |> 
     dplyr::filter(Sexes == 3)
