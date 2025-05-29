05de6494ee6219991f583103782001dfa6900fce
Chantel Wetzel - NOAA Federal <chantel.wetzel@noaa.gov>
2025-05-29 08:55:15 -0700

    add info to locate enviro index

--- data-raw/data_enviro_index.R
+++ data-raw/data_enviro_index.R
@@ -1,7 +1,9 @@
 # Environmental index
+# github repo: https://github.com/ntolimieri/sablefish-risk-table
+# "C:/Assessments/2025/sablefish-risk-table/Environmental-index-4Assessement-1993-2024.csv"
+# the data-raw/enviro_index is available on the network and google drive with the required csv file
 data_enviro_index <- read.csv(
-  "C:/Assessments/2025/sablefish-risk-table/Environmental-index-4Assessement-1993-2024.csv"
-) |>
+  here::here("data-raw", "enviro_index", "Environmental-index-4Assessement-1993-2024.csv")) |>
   dplyr::select(-Y_rec) |>
   dplyr::rename(
     obs = fit,
