49db1494f04759367128be15d8985323c29121bb
Aaron Berger <7946234+aaronmberger-nwfsc@users.noreply.github.com>
2025-05-30 15:35:09 -0700

    update enviro index and code for new model output and 5 year

--- data-raw/data_enviro_index.R
+++ data-raw/data_enviro_index.R
@@ -1,13 +1,13 @@
 # Environmental index
 # github repo: https://github.com/ntolimieri/sablefish-risk-table
-# "C:/Assessments/2025/sablefish-risk-table/Environmental-index-4Assessement-1993-2024.csv"
+# "C:/Assessments/2025/sablefish-risk-table/Environmental-index-4Assessement-1993-2019_predict_to-2024_errorCORRECTED.csv"
 # the data-raw/enviro_index is available on the network and google drive with the required csv file
 data_enviro_index <- read.csv(
-  here::here("data-raw", "enviro_index", "Environmental-index-4Assessement-1993-2024.csv")) |>
-  dplyr::select(-Y_rec) |>
+  here::here("data-raw", "enviro_index", "Environmental-index-4Assessement-1993-2019_predict_to-2024_errorCORRECTED.csv")) |>
+  dplyr::select(-Y_rec,-pred_int_lwr,-pred_int_upr,-se) |>
   dplyr::rename(
     obs = fit,
-    se_log = se
+    se_log = prediction_error
   ) |>
   dplyr::mutate(
     month = 1,
@@ -21,7 +21,7 @@ data_enviro_index <- read.csv(
   dplyr::relocate(
     index, .after = month
   ) |>
-  dplyr::filter(year > 2014)
+  dplyr::filter(year > 2019)
 
 write_named_csvs(
   data_enviro_index,
