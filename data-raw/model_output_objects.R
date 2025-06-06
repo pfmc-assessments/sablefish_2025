fb591dac78ecf680ae1ec4ff2b582f731e70288f
Chantel Wetzel - NOAA Federal <chantel.wetzel@noaa.gov>
2025-06-06 09:45:07 -0700

    add model output for the 2023 model

--- data-raw/model_output_objects.R
+++ data-raw/model_output_objects.R
@@ -1,20 +1,14 @@
 # 2023 Model for Comparisons
 model_dir <- here::here("model", "_bridging", "0_2023_model")
-model_output <- r4ss::SS_output(model_dir)
-save(model_output, file = file.path(model_dir, "model_2023_output.rda"))
+model_output_2023 <- r4ss::SS_output(model_dir)
+save(model_output_2023, file = here::here("data", "model_output_2023.rda"))
 
 # Base Model for the report
-model_name <- "0_init_hessian"
-model_dir <- here::here("model", model_name)
+model_name <- "8.34_fix_additional_selex_param_rec_dev_2"
+model_dir <-  here::here("model", "base_model", model_name)
 model_output <- r4ss::SS_output(model_dir)
-save(model_output, file = file.path(model_dir, "model_output.rda"))
+save(model_output, file = here::here("data", "model_output.rda"))
 r4ss::SS_plots(model_output)
 
-r4ss::table_all(
-  replist = model_output,
-  fleetnames = c(
-    "Trawl", "Hook-and-Line", "Pot",
-    "Triennial", "AKFSC Slope", "NWFSC Slope", "WCGBT"
-  )
-)
+r4ss::table_all(replist = model_output)
 r4ss::table_config(replist = model_output)
