0add62c4d63593412bb35e1db9b0c38279d85493
Chantel Wetzel - NOAA Federal <chantel.wetzel@noaa.gov>
2025-06-12 12:51:56 -0700

    add custom figure

--- data-exploration/plot_custom_r4ss_figures.R
+++ data-exploration/plot_custom_r4ss_figures.R
@@ -1,6 +1,6 @@
 library(r4ss)
 
-model <- SS_output(here::here("model", "base_model", "8.36_base_model - Copy"))
+model <- SS_output(here::here("model", "base_model", "8.36_base_model"))
 dir.create(here::here("model", "base_model", "8.36_base_model", "custom_plots"))
 SS_fitbiasramp(
   replist = model, 
@@ -23,4 +23,22 @@ SSplotPars(
 file.copy(
   from = here::here("model", "base_model", "8.36_base_model", "custom_plots", "parameter_distributions_pageNaN.png"),
   to = here::here("report", "figures")
+)
+SSplotComps(
+  replist = model,
+  kind = "AGE",
+  fleets = 7,
+  maxrows = 3,
+  maxcols = 1,
+  print = TRUE,
+  plotdir = here::here("model", "base_model", "8.36_base_model", "custom_plots")
+)
+SSplotComps(
+  replist = model,
+  kind = "AGE",
+  fleets = 8,
+  maxrows = 2,
+  maxcols = 1,
+  print = TRUE,
+  plotdir = here::here("model", "base_model", "8.36_base_model", "custom_plots")
 )
