fff21b8b8aa1cabbdb0f62caba14337d46c07cb9
Chantel Wetzel - NOAA Federal <chantel.wetzel@noaa.gov>
2025-07-07 13:42:57 -0700

    add rec index fit figure

--- presentation/model_and_diag/model_and_diag_plots.R
+++ presentation/model_and_diag/model_and_diag_plots.R
@@ -102,9 +102,71 @@ r4ss::SSplotComps(
 )
 
 #===============================================================================
-# Bridging
+# Recruitment Index
 #===============================================================================
 
-pwidth = 6.5,
-pheight = 5.0,
+data <- model_output$cpue |>
+  dplyr::filter(Fleet == 11) |>
+  dplyr::mutate(
+    Survey = "Recruitment Index",
+    year = Yr,
+    est = Obs,
+    sd = SE_input,
+    lwr = qnorm(0.025, mean = est, sd = sd),
+    upr = qnorm(0.975, mean = est, sd = sd)
+  )
 
+model_est <- model_output$cpue |>
+  dplyr::filter(Fleet == 11) |>
+  dplyr::select(Yr, Exp) |>
+  dplyr::rename(
+    year = Yr, 
+    est = Exp) |>
+  dplyr::mutate(
+    Survey = "Catchability * Rec. Dev."
+  )
+
+rec_est <- model_output$cpue |>
+  dplyr::filter(Fleet == 11) |>
+  dplyr::select(Yr, Vuln_bio) |>
+  dplyr::rename(
+    year = Yr, 
+    dev = Vuln_bio) |>
+  dplyr::mutate(
+    Survey = "Rec. Dev."
+  )
+
+ggplot2::ggplot(
+  data = data,
+  ggplot2::aes(
+    x = year,
+    y = est,
+    group = Survey,
+    colour = Survey,
+    fill = Survey
+  )
+) +
+  ggplot2::geom_point(size = 3) +
+  #ggplot2::geom_line(linewidth = 1) +
+  ggplot2::geom_errorbar(
+    ggplot2::aes(ymin = lwr, ymax = upr),
+    linewidth = 1
+  ) +
+  ggplot2::ylim(c(-4, 4)) +
+  ggplot2::geom_point(data = model_est, ggplot2::aes(x = year, y = est), color = "#008DA8", size = 4, shape = 17) +
+  ggplot2::geom_line(data = model_est, ggplot2::aes(x = year, y = est), color = "#008DA8", linewidth = 1) +
+  ggplot2::geom_point(data = rec_est, ggplot2::aes(x = year, y = dev), color = "grey", size = 4, shape = 15) +
+  ggplot2::geom_hline(yintercept = 0, linetype = "dashed", color = "black") +
+  ggplot2::theme_bw() +
+  ggplot2::theme(
+    legend.position = c(0.80, 0.15),
+    legend.title = ggplot2::element_text(size = 15), 
+    legend.text = ggplot2::element_text(size = 15),
+    axis.text = ggplot2::element_text(size = 20),
+    axis.title = ggplot2::element_text(size = 20)
+  ) +
+  nmfspalette::scale_color_nmfs(palette = "oceans", reverse = TRUE) +
+  ggplot2::xlab("Year") +
+  ggplot2::ylab("Recruitment Deviation") +
+  ggplot2::expand_limits(y = 0)
+ggplot2::ggsave(filename = here::here("presentation", "model_and_diag", "plots", "recruitment_index_fit.png"), width = 10, height = 10)
