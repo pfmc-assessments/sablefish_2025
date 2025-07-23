0b5d75a17ca50641ab63b91708d22d882202fcef
Chantel Wetzel - NOAA Federal <chantel.wetzel@noaa.gov>
2025-07-23 09:20:44 -0700

    update historical figures

--- R/plot_historical_ssb.R
+++ R/plot_historical_ssb.R
@@ -1,24 +1,72 @@
-plot_historical_ssb <- function(){
-    ggplot2::ggplot(historical_ssb)+
-        ggplot2::geom_line(ggplot2::aes(x=Year, y=Value, color=Assessment_Year)) +
-        ggplot2::geom_point(ggplot2::aes(x=Year, y=Value, color=Assessment_Year)) +
-        # ggplot2::facet_wrap(~Quantity, scales = "free_y") +
-        ggplot2::labs(
-            title = "",
-            x = "Model Year",
-            y = "Spawning Biomass (Metric tons)",
-            color = "Assessment Year"
-        ) +
-        ggplot2::scale_x_continuous(limits = c(1890, 2025), breaks=seq(1890, 2025, 20)) +
-        ggplot2::scale_y_continuous(limits = c(0, 300000), breaks=seq(0, 3e5, 5e4), labels=scales::comma)+
-        ggplot2::guides(
-            color = ggplot2::guide_legend(nrow = 1, byrow = TRUE)
-        ) +
-        ggplot2::theme_bw(base_size = 16) + 
-        ggplot2::theme(
-            legend.position = "bottom",
-            legend.box = "horizontal",
-            panel.grid.major = ggplot2::element_line(color = "grey90"),
-            panel.grid.minor = ggplot2::element_blank()
-        )
+#' Plot historical spawning outptu
+#'
+#' 
+#' @param  historical_ssb Dataframe created by data_historical-SSB.R
+#'
+plot_historical_ssb <- function(historical_ssb){
+
+  colors <- c("#0072B2", "#D55E00", "#CC79A7", "#009E73","#E69F00", "#F0E442", "#56B4E9", "#000000")
+  ggplot2::ggplot(historical_ssb |> dplyr::filter(Year >= 1890) |> dplyr::arrange(Assessment_Year) |> dplyr::mutate(Assessment_Year = as.factor(Assessment_Year)),
+        ggplot2::aes(x=Year, y=Value, color = Assessment_Year)) +
+    ggplot2::geom_line(linewidth = 1.5)  +
+    ggplot2::labs(
+        title = "",
+        x = "Year",
+        y = "Spawning Output (mt)",
+        color = "Assessment Year"
+    ) +
+    ggplot2::scale_color_manual(values = colors) + 
+    ggplot2::scale_x_continuous(limits = c(1890, 2025), breaks=seq(1890, 2025, 20)) +
+    ggplot2::scale_y_continuous(limits = c(0, 260000), breaks=seq(0, 3e5, 5e4), labels=scales::comma) +
+    #ggplot2::guides(
+    #    color = ggplot2::guide_legend(nrow = 1, byrow = TRUE)
+    #) +
+    ggplot2::theme_bw(base_size = 16) + 
+    ggplot2::theme(
+        legend.position = "bottom",
+        legend.box = "horizontal",
+        panel.grid.major = ggplot2::element_line(color = "grey90"),
+        panel.grid.minor = ggplot2::element_blank()
+    )
+}
+
+#' Plot historical stock status
+#'
+#' 
+#' @param  historical_ssb Dataframe created by data_historical-SSB.R
+#'
+plot_historical_depl <- function(historical_ssb){
+  
+  data <- historical_ssb |> 
+    dplyr::group_by(Assessment_Year) |>
+    dplyr::mutate(
+      depl = Value / Value[1],
+      Assessment_Year = as.factor(Assessment_Year)
+    ) |>
+    dplyr::arrange(Assessment_Year) |> 
+    dplyr::filter(Year >= 1890) 
+  colors <- c("#0072B2", "#D55E00", "#CC79A7", "#009E73","#E69F00", "#F0E442", "#56B4E9", "#000000")
+  ggplot2::ggplot(data, ggplot2::aes(x=Year, y=depl, color = Assessment_Year)) +
+    ggplot2::geom_line(linewidth = 1.5)  +
+    ggplot2::labs(
+      title = "",
+      x = "Year",
+      y = "Stock Status",
+      color = "Assessment Year"
+    ) +
+    ggplot2::geom_hline(yintercept = 0.40, linetype = "dashed", color = "black") + 
+    ggplot2::geom_hline(yintercept = 0.25, linetype = "dashed", color = "red") + 
+    ggplot2::scale_color_manual(values = colors) + 
+    ggplot2::scale_x_continuous(limits = c(1890, 2025), breaks=seq(1890, 2025, 20)) +
+    ggplot2::ylim(c(0, 1.30)) +
+    #ggplot2::guides(
+    #  color = ggplot2::guide_legend(nrow = 1, byrow = TRUE)
+    #) +
+    ggplot2::theme_bw(base_size = 16) + 
+    ggplot2::theme(
+      legend.position = "bottom",
+      legend.box = "horizontal",
+      panel.grid.major = ggplot2::element_line(color = "grey90"),
+      panel.grid.minor = ggplot2::element_blank()
+    )
 }
--- data-raw/data_historical-SSB.R
+++ data-raw/data_historical-SSB.R
@@ -1,61 +1,80 @@
 get_ssb_timeseries <- function(year){
-    fname <- file.path(here::here(), "model", "_bridging", paste0("0_",year,"_base_model"))
-    model <- r4ss::SS_output(fname, verbose=FALSE, printstats=FALSE)
-
+   fname <- file.path(here::here(), "model", "_bridging", paste0("0_",year,"_model"))
+    model <- r4ss::SS_output(fname, verbose = FALSE, printstats = FALSE)
     return(
-        model$timeseries[,c("Yr", "SpawnBio")] %>% as_tibble() %>%
-            dplyr::rename(
-                Year = Yr,
-                Value = SpawnBio
-            ) %>%
-            dplyr::mutate(
-                Quantity = "SSB (MT)",
-                Assessment_Year = year
-            ) %>%
-            dplyr::select(
-                Year,
-                Quantity,
-                Assessment_Year,
-                Value
-            ) %>%
-            dplyr::filter(Year <= year)
+        model$timeseries[,c("Yr", "SpawnBio")] |> 
+          tibble::as_tibble() |>
+          dplyr::rename(
+              Year = Yr,
+              Value = SpawnBio
+          ) |>
+          dplyr::mutate(
+              Quantity = "SSB (MT)",
+              Assessment_Year = year
+          ) |>
+          dplyr::select(
+              Year,
+              Quantity,
+              Assessment_Year,
+              Value
+          ) |>
+          dplyr::filter(Year <= year)
     )
 }
 
 file_path <- file.path(here::here(), "data-raw", "historical", "SIS_Timeseries_Sablefish_2005-2019_ForR.csv")
-
-data <- readr::read_csv(file_path) %>% tibble::as_tibble() %>% 
-    dplyr::select(Year, dplyr::contains("SSB")) %>%
-    dplyr::rename(
-        "SSB_2005_MT" = SSB.2005.mt,
-        "SSB_2007_MT" = SSB.2007.mt,
-        "SSB_2011_MT" = SSB.2011.mt,
-        "SSB_2015_MT" = SSB.2015.mt,
-    ) %>%
-    tidyr::pivot_longer(
-        2:ncol(.),
-        names_to = c("Quantity"),
-        values_to = "Value"
-    ) %>%
-    tidyr::separate(
-        Quantity,
-        into = c("Quantity", "Assessment_Year"),
-        sep = "_",
-        remove = TRUE
-    ) %>%
-    dplyr::mutate(
-        Year = as.integer(Year),
-        Assessment_Year = as.double(Assessment_Year),
-    )
+data <- readr::read_csv(file_path) |> 
+  tibble::as_tibble() |> 
+  dplyr::select(Year, dplyr::contains("SSB")) |>
+  dplyr::rename(
+      "SSB_2005_MT" = SSB.2005.mt,
+      "SSB_2007_MT" = SSB.2007.mt,
+      "SSB_2011_MT" = SSB.2011.mt,
+      "SSB_2015_MT" = SSB.2015.mt,
+  ) |>
+  tidyr::pivot_longer(
+      cols = 2:5,
+      names_to = c("Quantity"),
+      values_to = "Value"
+  ) |>
+  tidyr::separate(
+      Quantity,
+      into = c("Quantity", "Assessment_Year"),
+      sep = "_",
+      remove = TRUE
+  ) |>
+  dplyr::mutate(
+      Year = as.integer(Year),
+      Assessment_Year = as.double(Assessment_Year),
+  ) |>
+  dplyr::filter(!is.na(Value))
 
 ssb19 <- get_ssb_timeseries(2019)
 ssb21 <- get_ssb_timeseries(2021)
 ssb23 <- get_ssb_timeseries(2023)
+base_25 <- r4ss::SS_output(dir = here::here("model", "base_model", "8.36_base_model")) 
+ssb25 <- base_25$timeseries[,c("Yr", "SpawnBio")] |> 
+  tibble::as_tibble() |>
+  dplyr::rename(
+    Year = Yr,
+    Value = SpawnBio
+  ) |>
+  dplyr::mutate(
+    Quantity = "SSB (MT)",
+    Assessment_Year = 2025
+  ) |>
+  dplyr::select(
+    Year,
+    Quantity,
+    Assessment_Year,
+    Value
+  ) |>
+  dplyr::filter(Year <= 2025)
 
-historical_ssb <- dplyr::bind_rows(data, ssb19, ssb21, ssb23) %>%
-    dplyr::mutate(
-        Assessment_Year = factor(Assessment_Year)
-    )
+historical_ssb <- dplyr::bind_rows(data, ssb19, ssb21, ssb23,ssb25) |>
+  dplyr::mutate(
+      Assessment_Year = factor(Assessment_Year)
+  )
 
 write_named_csvs(
     historical_ssb,
